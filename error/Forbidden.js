6035f3c56191ccff085b78e1830e29456601b7ab
XVI-IX <oladoja14@gmail.com>
2023-06-13 00:16:02 +0100

    Add: Update Errors, add forbidden error

---
+++ error/Forbidden.js
@@ -0,0 +1,13 @@
+const { StatusCodes } = require("http-status-codes");
+const { CustomError } = require("./customError");
+
+class ForbiddenError extends CustomError {
+  constructor(message) {
+    super(message);
+    this.statusCode = StatusCodes.FORBIDDEN;
+  }
+}
+
+module.exports = {
+  ForbiddenError
+}
---
+++ error/index.js
@@ -0,0 +1,12 @@
+const BadRequestError = require("./BadRequest");
+const NotFoundError = require("./NotFound");
+const UnAuthenticatedError = require("./Unauthenticated");
+const ForbiddenError = require("./Forbidden");
+
+
+module.exports = {
+  BadRequestError,
+  NotFoundError,
+  UnAuthenticatedError,
+  ForbiddenError,
+}
