b8f302fedb36214bde83d7064ac52094d5efcf2a
XVI-IX <oladoja14@gmail.com>
2023-12-29 17:01:17 +0100

    Add: swagger docs

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const {
   uploadImage,
   getImageInfo
 } = require("./utils/cloudinary");
+const swaggerJSDoc = require("swagger-jsdoc");
+const swaggerUI = require("swagger-ui-express");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -38,6 +40,40 @@ app.use(passport.session());
 app.get("/", (req, res) => {
   res.send("Welcome to Social");
 });
+
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "Social API",
+      version: "0.1.0",
+      description: " This project involves building a social media platform that allows users to connect, share content, and interact with posts. The platform provides features such as user profiles, news feeds, and comment sections.",
+      license: {
+        name: "",
+        url: ""
+      },
+      contact: {
+        name: "XVI-IX",
+        email: "oladoja14@gmail.com"
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000"
+      },
+      {
+        url: "https://social-cgx9.onrender.com/"
+      },
+    ],
+  },
+  apis: ["./routes/*.js"],
+}
+
+const specs = swaggerJSDoc(options);
+app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(specs, {
+  explorer: true
+}))
+
 
 app.use(authRouter);
 app.use(authMiddleware, userRouter);
--- routes/user.js
+++ routes/user.js
@@ -6,6 +6,72 @@ const {
   updateProfile
 } = require("../controllers/user");
 
+/**
+ * @swagger
+ * components:
+ *  schemas:
+ *    User:
+ *      type: object
+ *      required:
+ *        - username
+ *        - email
+ *        - password
+ *        - phone_number
+ *      properties:
+ *        _id:
+ *          type: string
+ *          description: The auto-generated id of the user
+ *        username:
+ *          type: string
+ *          description: username provided by the user
+ *        first_name:
+ *          type: string
+ *          description: User's first name
+ *        last_name:
+ *          type: string
+ *          description: User's last name
+ *        other_name:
+ *          type: string
+ *          description: User's other name
+ *        email:
+ *          type: string
+ *          description: user's email address
+ *        password:
+ *          type: string
+ *          description: user's password. This will be hashed before being saved.
+ *        profile_img_url:
+ *          type: string
+ *          description: URL to user's profile image
+ *      example:
+ *          username: nabardd,
+ *          first_name: David-Daniel,
+ *          last_name: Ojebiyi,
+ *          other_name: Oluwaponmile,
+ *          email: oladoja14@gmail.com,
+ *          password: VictoryMonkey0*,
+ *          phone_number: +234901234567
+ */
+
+/**
+ * @swagger
+ * tags:
+ *  name: Social
+ *  description: The Social API
+ * /profile:
+ *  get:
+ *    summary: Get user's profile.
+ *    tags: [User]
+ *    responses:
+ *      200:
+ *        description: the retrieved user data
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/User'
+ *  put:
+ *    summary: Update user's profile
+ *    tags: [User]
+ */
 router.get("/profile", getProfile);
 router.patch("/profile", updateProfile);
 
